f4ba9825c6f543bea0982f2c5da634f12d734f59
manitu85 <manitu.psy@gmail.com>
2020-09-01 17:04:52 +0200

    #

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '59800b11806d907a002b8d57d6968ef6'),
+    },
+  },
+});
