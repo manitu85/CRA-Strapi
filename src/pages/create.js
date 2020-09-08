c8e9215ba6d6ca4fb192a9e4083ff55723f88bf5
manitu85 <manitu.psy@gmail.com>
2020-09-08 19:49:38 +0200

    #

--- src/pages/create.js
+++ src/pages/create.js
@@ -4,41 +4,62 @@ const Create = () => {
 
   const [description, setDescription] = useState('')
   const [file, setFile] = useState(null)
+  const [error, setError] = useState('')
 
   // console.log('FILE', file);
   
-
   const handleSubmit = async e => {
     e.preventDefault()
+
+    if(!description) {
+      setError('Please add description')
+      return
+    }
+
+    if(!file) {
+      setError('Please add an image')
+      return
+    }
 
     const formData = new FormData()
     formData.append('data', JSON.stringify({ description }))
     formData.append('files.image', file)
 
-    const res = await fetch('http://localhost:1337/posts', {
-      method: 'POST',
-      body: formData
-    }) 
+    try{
+      const res = await fetch('http://localhost:1337/posts', {
+        method: 'POST',
+        body: formData
+      }) 
 
-    const data = await res.json()
-    console.log('DATA', data);
-    
+      const data = await res.json()
+      console.log('DATA', data);
+    }catch(err){
+      console.log('Exception', err);
+      setError(err)
+    }
+
   }
 
   return(
     <div className='Create'>
       <h2>Create</h2>
 
+      {error && <p>{error}</p>}
+
       <form onSubmit={handleSubmit}>
         <input
           placeholder='Description'
           value={description}
-          onChange={e => setDescription(e.target.value)}
+          onChange={e => {
+            setError('')  
+            setDescription(e.target.value)}}
         />{' '}
         <input 
           type='file'
           placeholder='Add a File'
-          onChange={e => setFile(e.target.files[0])}
+          onChange={e => {
+            setError('')
+            setFile(e.target.files[0])}}
         />
         <button>Submit</button>
       </form>
